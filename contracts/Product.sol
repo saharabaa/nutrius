273ae1fd2bd51323b8015684881a0b8c0f697b74
Sahar Ahmed <sahar2015@my.fit.edu>
2017-11-19 23:43:39 -0500

    Added passing ingredients to product contract and fetching ingredients

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -4,16 +4,26 @@ pragma solidity ^0.4.18;
 contract Product {
 
     string name;
-
+    mapping (bytes32 => string) public ingredients;
     function Product() public {
 
     }
 
-    function setName(string _name) public {
+    function setName(string _name, string callback) public {
         name = _name;
     }
 
-    function getName() public returns(string) {
+    function getName() public view returns(string) {
         return name;
+    }
+
+    function setIngredients(string _name, string _qty) public {
+        bytes32 hash = sha3(_name);
+        ingredients[hash] = _qty;
+    }
+
+    function getIngredient(string _index) public returns(string) {
+        bytes32 hash = sha3(_index);
+        return ingredients[hash];
     }
 }
--- test/product.js
+++ test/product.js
@@ -6,11 +6,23 @@ contract('Product', function(accounts) {
     return Product.deployed().then(function(instance) {
       productInstance = instance;
 
-      return productInstance.setName('Synflex');
+      return productInstance.setName('Synflex 1500', []);
     }).then(function() {
       return productInstance.getName.call();
     }).then(function(storedData) {
-      assert.equal(storedData, 'Synflex', "The name of product was not stored");
+      assert.equal(storedData, 'Synflex 1500', "The name of product was not stored");
+    });
+  });
+
+  it("...should assert ingredients.", function() {
+    return Product.deployed().then(function(instance) {
+      productInstance = instance;
+
+      return productInstance.setIngredients('glucosamine', '1500kg');
+    }).then(function() {
+      return productInstance.getIngredient.call('glucosamine');
+    }).then(function(storedData) {
+      assert.equal(storedData, '1500kg', "Did not store ingredients");
     });
   });
 });
